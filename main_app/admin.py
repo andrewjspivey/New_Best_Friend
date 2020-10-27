4780eba3a2e27be5766330c82b598bdfd041a5d5
andrewjspivey <12spivey@gmail.com>
2020-10-27 11:21:33 -0700

    adds provider model and connects to dog model

--- main_app/admin.py
+++ main_app/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Dog
+from .models import Provider
 
 # Register your models here
 admin.site.register(Dog)
+admin.site.register(Provider)
--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,20 @@ from django.db import models
 
 # Create your models here.
 
+
+
+class Provider(models.Model):
+    name = models.CharField(max_length=75)
+    location = models.CharField(max_length=75)
+    description = models.TextField(max_length=500)
+    phone = models.CharField(max_length=15)
+    website = models.CharField(max_length=100)
+    image = models.CharField(max_length=200)
+    adoptionProcess = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
 class Dog(models.Model):
     name = models.CharField(max_length=50)
     location= models.CharField(max_length=50, null=True)
@@ -18,6 +32,11 @@ class Dog(models.Model):
     )
     gender = models.CharField(max_length=1, choices=DOG_GENDER, null=True)
     neutured = models.CharField(max_length=1, choices=IS_NEUTURED, null=True)
-    image = models.CharField(max_length=200, default='photo.jpg')
+    image = models.CharField(max_length=200, default="photo.jpg")
+    story = models.TextField(max_length=700, default="story")
+    provider = models.ForeignKey(Provider, on_delete=models.CASCADE, default="none")
+
+    def __str__(self):
+        return self.name
 
 
